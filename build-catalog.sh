890f8d016e421c75bba01f6e722edc994e3e751b
aokeeffe <a.okeeffe@sprinteins.com>
2020-04-13 16:31:36 +0200

    Sort the find output

--- build-catalog.sh
+++ build-catalog.sh
@@ -8,8 +8,10 @@ hash ghead 2>/dev/null && head="ghead" || head="head"
 
 ${head} -n -1 catalog-template.setup > arlos-projects.setup
 
-find projects -name "*.setup" \
-	-exec echo '  <project href="https://arlol.github.io/oomph-setups/'"{}"'#/"/>' \
-    	>> arlos-projects.setup \;
+find projects -name "*.setup" -print0 \
+    | sort --zero-terminated \
+    | xargs -0 -I % \
+    	echo '  <project href="https://arlol.github.io/oomph-setups/'"%"'#/"/>' \
+    		>> arlos-projects.setup \;
 
 echo '</setup:ProjectCatalog>' >> arlos-projects.setup
