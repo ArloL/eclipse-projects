293527844683bf76de74ac5d2d3d171df328056f
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-04-24 13:29:12 +0200

    Change from oomph-setups to eclipse-projects

--- build-catalog.sh
+++ build-catalog.sh
@@ -6,12 +6,12 @@ set -o xtrace
 
 hash ghead 2>/dev/null && head="ghead" || head="head"
 
-${head} -n -1 catalog-template.setup > arlos-projects.setup
+${head} -n -1 catalog-template.setup > eclipse-projects.setup
 
 find projects -name "*.setup" -print0 \
     | sort --zero-terminated \
     | xargs -0 -I % \
-    	echo '  <project href="https://arlol.github.io/oomph-setups/'"%"'#/"/>' \
-    		>> arlos-projects.setup \;
+    	echo '  <project href="https://arlol.github.io/eclipse-projects/'"%"'#/"/>' \
+    		>> eclipse-projects.setup \;
 
-echo '</setup:ProjectCatalog>' >> arlos-projects.setup
+echo '</setup:ProjectCatalog>' >> eclipse-projects.setup
