e6fefb8a6593d80d025902f478c4edaf8543321a
Arlo O'Keeffe <tiger@k5d.de>
2022-08-13 19:42:34 +0200

    refactor build-catalog.sh

--- build-catalog.sh
+++ build-catalog.sh
@@ -5,14 +5,18 @@ set -o errexit
 set -o xtrace
 
 hash ghead 2>/dev/null && head="ghead" || head="head"
+hash gxargs 2>/dev/null && xargs="gxargs" || xargs="xargs"
 
-${head} -n -1 catalog-template.setup > eclipse-projects.setup
+{
+    ${head} -n -1 catalog-template.setup
 
-find projects -name "*.setup" -print0 \
-    | sort --zero-terminated \
-    | xargs -0 -I % \
-    	echo '  <project href="https://arlol.github.io/eclipse-projects/'"%"'#/"/>' \
-    		>> eclipse-projects.setup \;
+    find projects -name "*.setup" -print0 \
+        | sort --zero-terminated \
+        | ${xargs} --null -I % \
+        echo '<project href="https://arlol.github.io/eclipse-projects/%#/"/>'
 
-echo "<!-- $(date) -->" >> eclipse-projects.setup
-echo '</setup:ProjectCatalog>' >> eclipse-projects.setup
+    echo "<!-- $(date) -->"
+
+    echo '</setup:ProjectCatalog>'
+
+} > eclipse-projects.setup
